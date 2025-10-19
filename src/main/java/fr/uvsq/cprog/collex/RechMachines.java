b05671e6dd85199a983ecf76b28033b834c4a9b6
Wissem Ameziane <wissem.ameziane4@gmail.com>
2025-10-19 12:04:05 +0200

    Classe rechercher les machines

---
+++ src/main/java/fr/uvsq/cprog/collex/RechMachines.java
@@ -0,0 +1,37 @@
+package fr.uvsq.cprog.collex;
+package fr.uvsq.cprog.collex.Commande;
+package fr.uvsq.cprog.collex.Dns;
+package fr.uvsq.cprog.collex.DnsItem;
+
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class RechMachines implements Commande {
+    private final Dns dns;
+    private final String domaine;
+    private final boolean byIp;
+
+    public RechMachines(final Dns dns, final String domaine, final boolean byIp) {
+        this.dns = dns;
+        this.domaine = domaine;
+        this.byIp = byIp;
+    }
+
+    @Override
+    public String execute() {
+        List<DnsItem> items = dns.getItems(domaine);
+        if (items.isEmpty()) {
+            return "";
+        }
+        if (byIp) {
+            Collections.sort(items, Comparator.comparing(i -> i.getIp()));
+        } else {
+            Collections.sort(items, Comparator.comparing(i -> i.getNom().getMachine().toLowerCase()));
+        }
+        return items.stream()
+                .map(i -> i.getIp().asString() + " " + i.getNom().asString())
+                .collect(Collectors.joining(System.lineSeparator()));
+    }
+}
